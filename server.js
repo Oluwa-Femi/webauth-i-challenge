3cb2627db0d871e1eab9747fb4fcee5f84e0ad1d
Oluwa-Femi <oluwatolablessing@gmail.com>
2019-11-12 14:17:02 +0000

    Users router set

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 
 const db = require('./data/db-config');
+const UsersRouter = require('./users/users-router');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use('/api/users', UsersRouter)
 
 module.exports = server;
